de4a4f7da90fa82ab480ed9395f7dcaf251c502d
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-03-13 15:24:52 +0200

    Adding saved page

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,5 +1,31 @@
+import { Models } from "appwrite";
+import { useGetCurrentUser } from "@/lib/react-query/queries";
+import TopPage from "@/components/shared/TopPage";
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import saveIcon from "/assets/icons/save.svg";
+
 function Saved() {
-    return <div>Saved</div>;
+    const { data: user, isPending: isLoadingUser } = useGetCurrentUser();
+    const savedPosts = user?.save.map((item: Models.Document) => item.post);
+
+    return (
+        <div className="saved-container">
+            <TopPage icon={saveIcon} alt="save">
+                Saved Posts
+            </TopPage>
+
+            {isLoadingUser ? (
+                <Loader />
+            ) : (
+                <GridPostList
+                    posts={savedPosts}
+                    showUser={false}
+                    showStats={false}
+                />
+            )}
+        </div>
+    );
 }
 
 export default Saved;
